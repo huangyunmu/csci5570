aa74d0b2b5c092154bbd287fbe7c570621416d86
huangyunmu <andyhuangyunmu@gmail.com>
2017-10-27 22:24:44 +0800

    Test 2224

--- lib/data_loader_test.cpp
+++ lib/data_loader_test.cpp
@@ -1,7 +1,8 @@
 #include "glog/logging.h"
 #include "gtest/gtest.h"
 #include "parser.hpp"
-#include "data_loader.cpp"
+#include "lib/labeled_sample.hpp"
+#include "lib/abstract_data_loader.hpp"
 namespace csci5570 {
 namespace {
 
