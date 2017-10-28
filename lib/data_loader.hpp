6480cb5dc11bc1fdd8558fb0913adf8bee902336
huangyunmu <andyhuangyunmu@gmail.com>
2017-10-28 11:22:26 +0800

    Test 1122

--- lib/data_loader.hpp
+++ lib/data_loader.hpp
@@ -1,17 +1,15 @@
+#pragma once
 #include "lib/abstract_data_loader.hpp"
-
 #include <thread>
-
 #include "boost/utility/string_ref.hpp"
-#include "glog/logging.h"
-#include "gtest/gtest.h"
-
 #include "base/serialization.hpp"
 #include "io/coordinator.hpp"
 #include "io/hdfs_assigner.hpp"
 #include "io/hdfs_file_splitter.hpp"
 #include "io/line_input_format.hpp"
-
+#include "lib/abstract_data_loader.hpp"
+#include "lib/labeled_sample.hpp"
+#include "lib/parser.hpp"
 namespace csci5570 {
 namespace lib {
 
--- lib/data_loader_test.cpp
+++ lib/data_loader_test.cpp
@@ -2,9 +2,7 @@
 #include <vector>
 #include "glog/logging.h"
 #include "gtest/gtest.h"
-#include "lib/abstract_data_loader.hpp"
-#include "lib/labeled_sample.hpp"
-#include "lib/parser.hpp"
+
 namespace csci5570 {
 
 class TestDataLoader : public testing::Test {
