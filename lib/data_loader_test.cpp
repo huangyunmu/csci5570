0aabf7e66bb03d95075aec5236b84620cdd1fe57
huangyunmu <andyhuangyunmu@gmail.com>
2017-10-28 11:48:07 +0800

    Test 1148

--- lib/data_loader_test.cpp
+++ lib/data_loader_test.cpp
@@ -4,7 +4,7 @@
 #include "gtest/gtest.h"
 
 namespace csci5570 {
-
+namespace {
 class TestDataLoader : public testing::Test {
  public:
   TestDataLoader() {}
@@ -17,9 +17,9 @@ class TestDataLoader : public testing::Test {
 
 TEST_F(TestDataLoader, LoadData) {
   using DataStore = std::vector<int>;
-  using Sample=lib::LabeledSample<int, int>;
-  using Parser=lib::Parser<Sample, DataStore>;
-  using Parse=int;
+  using Sample = lib::LabeledSample<int, int>;
+  using Parser = lib::Parser<Sample, DataStore>;
+  using Parse = int;
   // using Parse=std::function<Sample(boost::string_ref, int)>;
   DataStore data_store;
   Sample svm_sample();
@@ -27,8 +27,8 @@ TEST_F(TestDataLoader, LoadData) {
   Parse svm_parse;
   int n_features = 10;
   std::string url = "hdfs:///datasets/classification/a9";
-  lib::DataLoader<Sample,DataStore> data_loader;
+  lib::DataLoader<Sample, DataStore> data_loader;
   data_loader.load<Parse>(url, n_features, svm_parse, &data_store);
 }
-
+}
 }  // namespace csci5570
