057d7ee6715223610b8f75fdb83abe7ee60f10c3
huangyunmu <andyhuangyunmu@gmail.com>
2017-11-04 16:44:10 +0800

    Layout change

--- lib/parser.hpp
+++ lib/parser.hpp
@@ -24,19 +24,18 @@ class Parser {
     // so far we tried all the tree and found std::strtok_r is fastest :)
     Sample temp_sample = SVMSample();
     boost::tokenizer<> tok(line);
-    int count=0;
+    int count = 0;
     for (boost::tokenizer<>::iterator beg = tok.begin(); beg != tok.end(); ++beg) {
-      if(count==0){
-        if(line.substr(0,1)=="+"){
-          LOG(INFO)<<"Positive";
+      if (count == 0) {
+        if (line.substr(0, 1) == "+") {
+          LOG(INFO) << "Positive";
           // temp_sample.y_=1;
-        }
-        else{
-          LOG(INFO)<<"Negative";
+        } else {
+          LOG(INFO) << "Negative";
           // temp_sample.y_=0;
         }
       }
-      if(count%2==1){
+      if (count % 2 == 1) {
         // int index=stoi(*beg);
         // temp_sample.x_.push_back(index);
         LOG(INFO) << *beg;
