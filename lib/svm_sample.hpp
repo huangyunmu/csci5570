ea8fbc7c480ea09f10d24527edde80bdee45383b
huangyunmu <andyhuangyunmu@gmail.com>
2017-11-04 16:59:49 +0800

    Good 1700

--- lib/svm_sample.hpp
+++ lib/svm_sample.hpp
@@ -11,11 +11,11 @@ namespace lib {
 class SVMSample : public LabeledSample<third_party::SArray<int>, int> {
 public:
   std::string toString(){
-    std::string result="";
+    std::string result="Feature: ";
     for (int i = 0; i < x_.size(); i++) {
         result=result+" "+std::to_string(x_[i]);
       }
-    result=result+" "+std::to_string(y_);
+    result=result+"Label: "+std::to_string(y_);
     return result;
   }
   void test() {
