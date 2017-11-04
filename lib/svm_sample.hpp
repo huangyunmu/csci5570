6ccf8b8c87b3e4e08caf254a2e73355952552a54
huangyunmu <andyhuangyunmu@gmail.com>
2017-11-04 16:57:44 +0800

    Test 1657

--- lib/svm_sample.hpp
+++ lib/svm_sample.hpp
@@ -12,7 +12,7 @@ class SVMSample : public LabeledSample<third_party::SArray<int>, int> {
 public:
   std::string toString(){
     std::string result="";
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < x_.size(); i++) {
         result=result+" "+std::to_string(x_[i]);
       }
     result=result+" "+std::to_string(y_);
