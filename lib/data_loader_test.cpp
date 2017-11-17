04813441627826de1015c1e8ff2959ee374cc380
huangyunmu <andyhuangyunmu@gmail.com>
2017-11-17 17:09:10 +0800

    Comment all data loader

--- lib/data_loader_test.cpp
+++ lib/data_loader_test.cpp
@@ -16,20 +16,20 @@ class TestDataLoader : public testing::Test {
 };
 
 TEST_F(TestDataLoader, LoadData) {
-  using DataStore = std::vector<lib::SVMSample>;
-  using Parser = lib::Parser<lib::SVMSample, DataStore>;
-  // using Parse = int;
-  using Parse =std::function<lib::SVMSample(boost::string_ref ,int)>;
+  // using DataStore = std::vector<lib::SVMSample>;
+  // using Parser = lib::Parser<lib::SVMSample, DataStore>;
+  // // using Parse = int;
+  // using Parse =std::function<lib::SVMSample(boost::string_ref ,int)>;
  
-  // using Parse=std::function<Sample(boost::string_ref, int)>;
-  DataStore data_store;
-  lib::SVMSample svm_sample;
-  // Parser svm_parser();
-  Parse svm_parse=Parser->parse_libsvm();
-  int n_features = 10;
-  std::string url = "hdfs:///datasets/classification/a9";
-  lib::DataLoader<lib::SVMSample, DataStore> data_loader;
-  data_loader.load<Parse>(url, n_features, svm_parse, &data_store);
+  // // using Parse=std::function<Sample(boost::string_ref, int)>;
+  // DataStore data_store;
+  // lib::SVMSample svm_sample;
+  // // Parser svm_parser();
+  // Parse svm_parse=Parser->parse_libsvm();
+  // int n_features = 10;
+  // std::string url = "hdfs:///datasets/classification/a9";
+  // lib::DataLoader<lib::SVMSample, DataStore> data_loader;
+  // data_loader.load<Parse>(url, n_features, svm_parse, &data_store);
   // data_loader.test();
 }
 
