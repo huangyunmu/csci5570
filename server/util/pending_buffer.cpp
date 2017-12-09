6c437e61886f4d8fcaa43481c08327103578942d
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-09 16:56:10 +0800

    Pending Buffer

--- server/util/pending_buffer.cpp
+++ server/util/pending_buffer.cpp
@@ -3,15 +3,18 @@
 namespace csci5570 {
 
 std::vector<Message> PendingBuffer::Pop(const int clock) {
-  // TODO
+  std::unordered_map<int, std::vector<Message>>::iterator iter;
+  iter = map_.find(clock);
+  vector<Message> temp;
+  if (iter != map_.end()) {
+    temp = this->map_[clock];
+    map_.erase(iter);
+  }
+  return temp;
 }
 
-void PendingBuffer::Push(const int clock, Message& msg) {
-  // TODO
-}
+void PendingBuffer::Push(const int clock, Message& msg) { this->map_[clock].push_back(msg); }
 
-int PendingBuffer::Size(const int progress) {
-  // TODO
-}
+int PendingBuffer::Size(const int progress) { return this->map_[progress].size(); }
 
 }  // namespace csci5570
