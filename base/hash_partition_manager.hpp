14d0221e81b6559ac051d0f9dd7f2ca760ffc9d1
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-19 00:16:36 +0800

    Test 0016

--- base/hash_partition_manager.hpp
+++ base/hash_partition_manager.hpp
@@ -13,7 +13,7 @@ namespace csci5570 {
 class HashPartitionManager : public AbstractPartitionManager {
  public:
   HashPartitionManager(const std::vector<uint32_t>& server_thread_ids);
-  int32_t JumpConsistentHash(uint64_t key, int32_t num_buckets);
+  int32_t JumpConsistentHash(uint64_t key, int32_t num_buckets) const;
   void Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) const override;
 
   void Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced) const override;
