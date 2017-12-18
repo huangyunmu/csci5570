1350069ff3556228263aac9330c3dd2123c28f44
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-19 01:13:52 +0800

    Test 0113

--- base/hash_partition_manager_test.cpp
+++ base/hash_partition_manager_test.cpp
@@ -32,11 +32,20 @@ TEST_F(TestHashPartitionManager, SliceKeys) {
 }
 
 TEST_F(TestHashPartitionManager, SliceKVs) {
-  HashPartitionManager pm({0, 1, 2});
+  HashPartitionManager pm({0, 1, 3});
   third_party::SArray<Key> keys({1, 2, 3, 4, 5, 6, 7, 8, 9});
   third_party::SArray<double> vals({.9, .8, .7, .6, .5, .4, .3, .2, .1});
   std::vector<std::pair<int, AbstractPartitionManager::KVPairs>> sliced;
   pm.Slice(std::make_pair(keys, vals), &sliced);
+  LOG(INFO) << sliced.size();
+  for (int i = 0; i < sliced.size(); i++) {
+    LOG(INFO) << "node id:" << sliced[i].first;
+    if (sliced[i].second.first.size() > 0) {
+      for (int j = 0; j < sliced[i].second.first.size(); j++) {
+        LOG(INFO) << "Key:" << sliced[i].second.first[j] << "Values:" << sliced[i].second.second[j];
+      }
+    }
+  }
 }
 
 }  // namespace csci5570
