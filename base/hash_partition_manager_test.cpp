e3d6be97db50afe97c21bf0b0107326f0163a434
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-19 00:57:11 +0800

    Test 0057

--- base/hash_partition_manager_test.cpp
+++ base/hash_partition_manager_test.cpp
@@ -16,9 +16,27 @@ TEST_F(TestHashPartitionManager, Init) { HashPartitionManager pm({0, 1, 2}); }
 
 TEST_F(TestHashPartitionManager, SliceKeys) {
   HashPartitionManager pm({0, 1, 2});
-  third_party::SArray<Key> keys({2, 8, 9});
+  third_party::SArray<Key> keys({1, 2, 3, 4, 5, 6, 7, 8, 9});
   std::vector<std::pair<int, AbstractPartitionManager::Keys>> sliced;
   pm.Slice(keys, &sliced);
+  LOG(INFO) << sliced.size();
+  for (int i = 0; i < sliced.size(); i++) {
+    LOG(INFO) << "node id:" << sliced[i].first;
+    if (sliced[i].second.size() > 0) {
+      LOG(INFO) << "keys:";
+      for (int j = 0; j < sliced[i].second.size(); j++) {
+        LOG(INFO) << sliced[i].second[j];
+      }
+    }
+  }
+  ASSERT_EQ(sliced.size(), 2);            // 2 slices for 2 servers
+  EXPECT_EQ(sliced[0].first, 0);          // key to server 0
+  EXPECT_EQ(sliced[1].first, 2);          // keys to server 2
+  ASSERT_EQ(sliced[0].second.size(), 1);  // key 2
+  ASSERT_EQ(sliced[1].second.size(), 2);  // keys 8, 9
+  EXPECT_EQ(sliced[0].second[0], 2);
+  EXPECT_EQ(sliced[1].second[0], 8);
+  EXPECT_EQ(sliced[1].second[1], 9);
 }
 
 TEST_F(TestHashPartitionManager, SliceKVs) {
