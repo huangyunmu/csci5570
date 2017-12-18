2936401a75c6f981cb0a1ef1cd6318886d05994d
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-18 20:48:52 +0800

    Test 2048

--- base/hash_partition_manager.cpp
+++ base/hash_partition_manager.cpp
@@ -2,26 +2,28 @@
 
 namespace csci5570 {
 
-	HashPartitionManager::HashPartitionManager(const std::vector<uint32_t>& server_thread_ids)
-		: AbstractPartitionManager(server_thread_ids) {
-		MD5_CTX ctx; 
-		LOG(INFO)<<MD5_Init(&ctx);
-		int num=0;
-		MD5_Update(&ctx, &num, 10);
-        // unsigned char outmd[16]; 
-	}
+HashPartitionManager::HashPartitionManager(const std::vector<uint32_t>& server_thread_ids)
+    : AbstractPartitionManager(server_thread_ids) {
+  MD5_CTX ctx;
+  unsigned char out[16];
+  LOG(INFO) << MD5_Init(&ctx);
+  int num = 0;
+  MD5_Update(&ctx, &num, 10);
+  MD5_Final(out, &ctx);
+  for (i = 0; i < 16; i < i++) {
+    printf("%02X", out[i]);
+  }
+}
 
-	void HashPartitionManager::Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) const {
-		const int keys_size = keys.size();//Num of keys
-	}
+void HashPartitionManager::Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) const {
+  const int keys_size = keys.size();  // Num of keys
+}
 
-	void HashPartitionManager::Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced)const  {
-		Keys keys = kvs.first;
-		third_party::SArray<double> vals = kvs.second;
-		const int keys_size = keys.size();//Num of keys
-		//LOG(INFO) <<"Keys size"<< keys_size;
-		
-	}
-
+void HashPartitionManager::Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced) const {
+  Keys keys = kvs.first;
+  third_party::SArray<double> vals = kvs.second;
+  const int keys_size = keys.size();  // Num of keys
+  // LOG(INFO) <<"Keys size"<< keys_size;
+}
 
 }  // namespace csci5570
