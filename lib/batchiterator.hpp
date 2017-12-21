cf8a2116566d2140fbd3a63a6e0575aededc7e47
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-22 01:44:39 +0800

    Rebuid the project

--- lib/batchiterator.hpp
+++ lib/batchiterator.hpp
@@ -10,16 +10,16 @@ namespace csci5570{
         BatchIterator(const std::vector<Sample>& data_store){
              datastore=data_store;
         }
-        std::pair<std::vector<int>,std::vector<Sample>> NextBatch(uint32_t size){
+        std::pair<std::vector<uint32_t>,std::vector<Sample>> NextBatch(uint32_t size){
             std::vector<Sample> data;
-            std::vector<int> keys;
+            std::vector<uint32_t> keys;
             for(int i=0;i<size;i++){
                 auto sample=datastore[rand()%datastore.size()];
                 data.push_back(sample);
                 auto& x=sample.x_;
                 for (auto& field : x) {
                     int key=field.first;
-                    std::vector<int>::iterator result = find(keys.begin(),keys.end(),key);
+                    std::vector<uint32_t>::iterator result = find(keys.begin(),keys.end(),key);
                     if(result==keys.end()){
                         keys.push_back(key);
                     }
