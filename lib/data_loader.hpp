a5129e2d694006ab9d8435ad0000fbba2c17d3f7
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-21 15:30:39 +0800

    Test 1530

--- lib/data_loader.hpp
+++ lib/data_loader.hpp
@@ -45,8 +45,8 @@ class DataLoader : public AbstractDataLoader<Sample, DataStore> {
     coordinator.serve();
     LOG(INFO) << "Coordinator begins serving";
     std::string input=url;
-    // std::thread worker_thread([&,input, hdfs_namenode_port, hdfs_namenode, &coordinator, worker_host, parse, &datastore] {
-    std::thread worker_thread([&] {
+    std::thread worker_thread([&,input, hdfs_namenode_port, hdfs_namenode, coordinator, worker_host, parse, datastore] {
+    // std::thread worker_thread([&] {
       // std::string input = "hdfs:///datasets/classification/a9";
       // std::string input = "hdfs:///datasets/classification/kdd12";
       // std::string input = url;
