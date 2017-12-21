5bacc83b5e4577dec1e7d2040db14100cdda763e
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-22 02:49:52 +0800

    Add additional feature test

---
+++ base/additional_feature_test.cpp
@@ -0,0 +1,17 @@
+#include "glog/logging.h"
+#include "gtest/gtest.h"
+
+#include "base/hash_partition_manager.hpp"
+#include "base/magic.hpp"
+
+namespace csci5570 {
+
+class TestAddtionalFeature : public testing::Test {
+ protected:
+  void SetUp() {}
+  void TearDown() {}
+};  // class TestHashPartitionManager
+
+TEST_F(TestAddtionalFeature, Init) {}
+
+}  // namespace csci5570
