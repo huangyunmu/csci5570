c29d3b614a099060210fea26d40a1d429ffb39dc
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-22 03:02:15 +0800

    Test 0302

--- base/additional_feature_test.cpp
+++ base/additional_feature_test.cpp
@@ -5,6 +5,9 @@
 #include "base/magic.hpp"
 #include "lib/data_loader.hpp"
 #include "lib/kdd_sample.hpp"
+
+#include "base/hash_partition_manager.hpp"
+#include "lib/batchiterator.hpp"
 namespace csci5570 {
 
 class TestAdditionalFeature : public testing::Test {
@@ -13,7 +16,7 @@ class TestAdditionalFeature : public testing::Test {
   void TearDown() {}
 };  // class TestHashPartitionManager
 
-TEST_F(TestAdditionalFeature, UsingRange) {
+TEST_F(TestAdditionalFeature, LoadHash) {
   using DataStore = std::vector<lib::KddSample>;
   using Parser = lib::Parser<lib::KddSample, DataStore>;
   using Parse = std::function<lib::KddSample(boost::string_ref, int)>;
@@ -24,7 +27,14 @@ TEST_F(TestAdditionalFeature, UsingRange) {
   std::string url = "hdfs:///datasets/classification/kdd12";
   lib::DataLoader<lib::KddSample, DataStore> data_loader;
   data_loader.load<Parse>(url, n_features, kdd_parse, &data_store);
-
+  BatchIterator<lib::KddSample> batch(data_store);
+  auto keys_data = batch.NextBatch(20000);
+  auto keys = keys_data.first;
+  HashPartitionManager pm({0, 1, 2});
+  std::vector<std::pair<int, AbstractPartitionManager::Keys>> sliced;
+  LOG(INFO) << "Start split";
+  pm.Slice(keys, &sliced);
+  LOG(INFO) << "End split";
 }
 
 }  // namespace csci5570
