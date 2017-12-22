1d44d30c6917e701c4ef54565e729658c3d44ea3
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-22 22:11:49 +0800

    Test 2211

--- scripts/start-svm.py
+++ scripts/start-svm.py
@@ -1,5 +1,5 @@
 import os
-num_of_nodes=2
+num_of_nodes=1
 cmd=""
 for i in range(0,num_of_nodes):
     cmd=cmd+"ssh 1155086998@proj"+str(10-i)+" & /data/opt/tmp/1155086998/pms/build/./TestSVM "+str(10-i)+" "+str(num_of_nodes)
@@ -7,5 +7,4 @@ for i in range(0,num_of_nodes):
         break
     if(i<num_of_nodes):
         cmd=cmd+" & "
-print cmd
 os.system(cmd)
