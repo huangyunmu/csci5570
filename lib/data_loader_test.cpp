7baab66be16e87a1af1cec2376f09ddfefe85f78
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-22 10:28:02 +0800

    Test 1027

--- lib/data_loader_test.cpp
+++ lib/data_loader_test.cpp
@@ -30,11 +30,11 @@ TEST_F(TestDataLoader, LoadSVMData) {
   int n_features = 10;
   lib::DataLoader<lib::SVMSample, DataStore> data_loader;
   std::string url = "hdfs:///datasets/classification/a9";  // Do not change
-  std::string hdfs_namenode = "proj10";                       // Do not change
-  std::string master_host = "proj10";                         // Set to worker name
-  std::string worker_host = "proj10";                         // Set to worker name
-  int hdfs_namenode_port = 9000;                              // Do not change
-  int master_port = 45743;                                    // Do not change
+  std::string hdfs_namenode = "proj10";                    // Do not change
+  std::string master_host = "proj10";                      // Set to worker name
+  std::string worker_host = "proj10";                      // Set to worker name
+  int hdfs_namenode_port = 9000;                           // Do not change
+  int master_port = 45743;                                 // Do not change
   // data_loader.load<Parse>(url, n_features, svm_parse, &data_store);
   data_loader.load<Parse>(url, hdfs_namenode, master_host, worker_host, hdfs_namenode_port, master_port, n_features,
                           svm_parse, &data_store);
@@ -58,7 +58,7 @@ TEST_F(TestDataLoader, LoadKddData) {
   std::string url = "hdfs:///datasets/classification/kdd12";  // Do not change
   std::string hdfs_namenode = "proj10";                       // Do not change
   std::string master_host = "proj10";                         // Set to worker name
-  std::string worker_host = "proj10";                         // Set to worker name
+  std::string worker_host = "proj5";                          // Set to worker name
   int hdfs_namenode_port = 9000;                              // Do not change
   int master_port = 45743;                                    // Do not change
   lib::DataLoader<lib::KddSample, DataStore> data_loader;
