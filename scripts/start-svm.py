b40431e48362b15e71c63ecad709817c47920d0d
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-23 14:23:51 +0800

    Test 1423

--- scripts/start-svm.py
+++ scripts/start-svm.py
@@ -3,12 +3,8 @@ num_of_nodes = 1
 cmd = ""
 port = 12425
 for i in range(0, num_of_nodes):
-    # cmd = cmd + "ssh 1155086998@proj" + str(10 - i) + "&&"
+    cmd = cmd + "ssh 1155086998@proj" + str(10 - i) + " & "
     cmd = cmd + "/data/opt/tmp/1155086998/pms/build/./TestSVM " + \
         str(10 - i) + " " + str(num_of_nodes) + " " + str(port)
-    if(i == 0):
-        break
-    # if(i<num_of_nodes):
-    #     cmd=cmd+" & "
 print cmd
 os.system(cmd)
