5b280e37d8dd137ab9d32a4847bd7a09c29777c1
huangyunmu <andyhuangyunmu@gmail.com>
2017-12-23 15:34:27 +0800

    Test 1534

--- scripts/start-svm.py
+++ scripts/start-svm.py
@@ -8,6 +8,7 @@ for i in range(0, num_of_nodes):
     cmd = cmd + "/data/opt/tmp/1155086998/csci5570/build/./TestSVM " + \
         str(10 - i) + " " + str(num_of_nodes) + " " + \
         str(port) + " " + str(master_port + i) + "&"
+    print cmd
     os.system(cmd)
     cmd = ""
 # print cmd
